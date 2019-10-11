6f94df86c3729f106771f7b5ee1142d73cc346a6
AJean144 <ajeanjacques42@gmail.com>
2019-10-11 00:11:21 -0400

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 GIPHY_TITLES = 10.times.map { Faker::Avatar.image }
+
+SPLIT = 0.4
+OFFSET = GIPHY_TITLES.length * SPLIT
+
+GIPHY_USERS = {
+  'andell@politech.io' => GIPHY_TITLES[0, OFFSET],
+  'jean@politech.io' => GIPHY_TITLES[OFFSET..-1]
+}
+
+GIPHY_USERS.each do |email, giphies|
+  user = User.create(email: email, password: 'password')
+  giphies.each { |title| user.giphies.build(title: title) }
+  user.save
+end
